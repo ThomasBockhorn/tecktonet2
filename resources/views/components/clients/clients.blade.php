3601c14729fa7c43208a6aaa140a92c4bcd20eec
Thomas Bockhorn <thomasbockhorn@Thomass-MBP.attlocal.net>
2020-06-02 04:52:45 -0500

    finished updating backend file structure for front

---
+++ resources/views/components/clients/clients.blade.php
@@ -0,0 +1,57 @@
+<!-- Clients Area -->
+<div id="clients" class="clients section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 col-sm-12 col-xs-12">
+						<div class="owl-carousel clients-slider">
+							<!-- Single Clients -->
+							<div class="single-clients">
+								<img src="img/client1.jpg" alt="#">
+							</div>
+							<!--/ End Single Client -->
+							<!-- Single Client -->
+							<div class="single-clients">
+								<img src="img/client2.jpg" alt="#">
+							</div>
+							<!--/ End Single Client -->
+							<!-- Single Client -->
+							<div class="single-clients">
+								<img src="img/client3.jpg" alt="#">
+							</div>
+							<!--/ End Single Client -->
+							<!-- Single Client -->
+							<div class="single-clients">
+								<img src="img/client4.jpg" alt="#">
+							</div>
+							<!--/ End Single Client -->
+							<!-- Single Client -->
+							<div class="single-clients">
+								<img src="img/client5.jpg" alt="#">
+							</div>
+							<!--/ End Single Client -->
+							<!-- Single Client -->
+							<div class="single-clients">
+								<img src="img/client6.jpg" alt="#">
+							</div>
+							<!--/ End Single Client -->
+							<!-- Single Client -->
+							<div class="single-clients">
+								<img src="img/client1.jpg" alt="#">
+							</div>
+							<!--/ End Single Client -->
+							<!-- Single Client -->
+							<div class="single-clients">
+								<img src="img/client2.jpg" alt="#">
+							</div>
+							<!--/ End Single Client -->
+							<!-- Single Client -->
+							<div class="single-clients">
+								<img src="img/client3.jpg" alt="#">
+							</div>
+							<!--/ End Single Client -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--/ End Clients Area -->

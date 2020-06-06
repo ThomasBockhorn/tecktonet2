e66218c3d99542a3081d67c06656eef400c49dc5
Thomas Bockhorn <thomasbockhorn@Thomass-MBP.attlocal.net>
2020-06-06 04:58:16 -0500

    changed the hamburger color and nav menu

--- resources/views/components/navbar/navbar.blade.php
+++ resources/views/components/navbar/navbar.blade.php
@@ -4,16 +4,14 @@
 			<div class="header-inner">
 				<div class="container">
 					<div class="row">
-						<div class="col-md-3 col-sm-2 col-xs-2 logo">
-							<a href="index.html"><img width="125" height="30" src="{{ asset('img/tecktonet_logo.png')}}" alt="logo.png"></a>
+						<div class="col-md-3 col-sm-4 col-xs-6 logo">
+							<a href="#slider"><img width="100%" height="100%" src="{{ asset('img/tecktonet_logo.png')}}" alt="logo.png"></a>
 						</div>
-						<div class="col-md-9 col-sm-10">
+						<div class="col-md-9 col-sm-8 col-xs-6">
 							<div class="mobile-menu"></div>
 							<nav class="navbar navbar-default">
 								<div class="collapse navbar-collapse">
 									<ul id="nav" class="nav navbar-nav">
-										<li class="current"><a href="#slider">home</a>
-										</li>
 										<li><a href="#Services">Services</a></li>
 										<li><a href="#latest-works">portfolio<i class="fa fa-caret-down" aria-hidden="true"></i></a>
 											<ul class="dropdown">
@@ -28,12 +26,6 @@
 											</ul>
 										</li>
 										<li><a href="#team">Team</a></li>
-										<li><a href="#">Pages <i class="fa fa-caret-down" aria-hidden="true"></i></a>
-											<ul class="dropdown">
-												<li><a href="404.html">Error 404</a></li>
-												<li><a href="mail-success.html">Mail Success</a></li>
-											</ul>
-										</li>
 										<li><a href="#contact">Contact</a></li>
 									</ul>
 								</div>

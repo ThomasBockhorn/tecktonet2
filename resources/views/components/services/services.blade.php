0fb152fcb5590f663db75e632b2a4c1b045e2a2f
Thomas Bockhorn <thomasbockhorn@Thomass-MBP.attlocal.net>
2020-06-10 12:00:13 -0500

    Updated service section

--- resources/views/components/services/services.blade.php
+++ resources/views/components/services/services.blade.php
@@ -4,8 +4,8 @@
 				<div class="row">
 					<div class="col-md-12">
 						<div class="section-title">
-							<h2>Exclusive <span>Services</span></h2>
-							<p>We create exclusive & creative Stuffs for our awesome clients</p>
+							<h2>Services</h2>
+							<p>Tecktonet.com can provide these valuable services to facilitate your website.</p>
 						</div>
 					</div>
 				</div>
@@ -13,43 +13,43 @@
 					<div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.4s">
 						<div class="single-service">
 							<i class="fa fa-briefcase" aria-hidden="true"></i>
-							<h3>Web Design</h3>
-							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+							<h3>Responive Design</h3>
+							<p>Building a website that can be viewed in multiple viewports.  This will give your website a wider reach.  Viewers will be able to use your site on a phone as on a desktop.</p>
 						</div>
 					</div>
 					<div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.5s">
 						<div class="single-service">
-							<i class="fas fa-file" aria-hidden="true"></i>
+							<i class="fas fa-code" aria-hidden="true"></i>
 							<h3>Web Development</h3>
-							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+							<p>Unlike content management systems that offer prebuilt solutions, I can build a website to your exact specifications.  Tecktonet.com custom solution will help you with your website needs.</p>
 						</div>
 					</div>
 					<div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.6s">
 						<div class="single-service">
-							<i class="fa fa-paper-plane" aria-hidden="true"></i>
-							<h3>Investment Plan</h3>
-							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+							<i class="fab fa-wordpress-simple" aria-hidden="true"></i>
+							<h3>WordPress custom themes</h3>
+							<p>Tecktonet.com can build WordPress themes if you want a custom solution for your blog or WooCommerce site.  All custom WordPress solutions will be responsive and easy to maintain.</p>
 						</div>
 					</div>
 					<div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.7s">
 					<div class="single-service">
-							<i class="fas fa-code" aria-hidden="true"></i>
-							<h3>Finance Support</h3>
-							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+							<i class="fas fa-cash-register" aria-hidden="true"></i>
+							<h3>Ecommerce solution</h3>
+							<p>From a simple landing page to a complex ecommerce store, Tecktonet.com can provide you a great solution to reach new customers and generate better revenue for your business. </p>
 						</div>
 					</div>
 					<div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.8s">
 						<div class="single-service">
 							<i class="fa fa-globe" aria-hidden="true"></i>
-							<h3>World Marketing</h3>
-							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+							<h3>SEO Marketing</h3>
+							<p>Tecktonet.com will provide a marketing campaign to entice foot traffic to your new site.  With a montly fee, I will place adds on social media like Facebook to make your website shine.</p>
 						</div>
 					</div>
 					<div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.9s">
 						<div class="single-service">
 							<i class="fas fa-laptop-code" aria-hidden="true"></i>
-							<h3>Perfect Solution</h3>
-							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+							<h3>Maintaince/Hosting</h3>
+							<p>For a monthly free, Tecktonet.com will maintain your site so that your website will always be at your best.  I will also provide hosting so your website has a solid footing.</p>
 						</div>
 					</div>
 

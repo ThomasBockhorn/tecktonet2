065a3c4510c7f7294ec4cd3fab68999959f32eac
Thomas Bockhorn <thomasbockhorn@Thomass-MBP.attlocal.net>
2020-06-17 15:52:08 -0500

    fixed navbar and size of images

--- resources/views/components/backend_components/navbar/navbar.blade.php
+++ resources/views/components/backend_components/navbar/navbar.blade.php
@@ -2,42 +2,24 @@
         <!-- Topbar header - style you can find in pages.scss -->
         <!-- ============================================================== -->
         <header class="topbar">
-            <nav class="navbar top-navbar navbar-toggleable-sm navbar-light">
+            <nav class="navbar top-navbar navbar-toggleable-sm navbar-light sticky-top">
                 <!-- ============================================================== -->
                 <!-- Logo -->
                 <!-- ============================================================== -->
                 <div class="navbar-header">
-                <div class="navbar-header">
                     <a class="navbar-brand" href="home">
                         <!-- Logo icon --><b>
                             <!-- Light Logo icon -->
-                            <img height="25px" width="34px" src="{{ asset('img/tecktonet_icon.png')}}" alt="homepage" class="light-logo" />
+                            <img height="50px" width="50px" src="{{ asset('img/tecktonet_icon.png')}}" alt="homepage" class="light-logo" />
                         </b>
                         <!--End Logo icon -->
                         <!-- Logo text --><span>
                          
                          <!-- Light Logo text -->    
-                         <img height="25px" width="120px" src="{{ asset('img/tecktonet_text.png')}}" class="light-logo" alt="homepage" /></span> </a>
+                         <img height="50px" width="200px" src="{{ asset('img/tecktonet_text.png')}}" class="light-logo" alt="homepage" /></span> </a>
                 </div>
                 <div class="navbar-collapse">
-                    <!-- ============================================================== -->
-                    <!-- toggle and nav items -->
-                    <!-- ============================================================== -->
-                    <ul class="navbar-nav mr-auto mt-md-0">
-                        <!-- This is  -->
-                        <li class="nav-item"> <a class="nav-link nav-toggler hidden-md-up text-muted waves-effect waves-dark" href="javascript:void(0)"><i class="mdi mdi-menu"></i></a> </li>
-                        <!-- ============================================================== -->
-                        <!-- Search -->
-                        <!-- ============================================================== -->
-                        <li class="nav-item hidden-sm-down search-box"> <a class="nav-link hidden-sm-down text-muted waves-effect waves-dark" href="javascript:void(0)"><i class="ti-search"></i></a>
-                            <form class="app-search">
-                                <input type="text" class="form-control" placeholder="Search & enter"> <a class="srh-btn"><i class="ti-close"></i></a> </form>
-                        </li>
-                    </ul>
-                    <!-- ============================================================== -->
-                    <!-- User profile and search -->
-                    <!-- ============================================================== -->
-                    <ul class="navbar-nav my-lg-0">
+                    <ul class="navbar-nav ml-auto my-lg-0">
                         <!-- ============================================================== -->
                         <!-- Profile -->
                         <!-- ============================================================== -->

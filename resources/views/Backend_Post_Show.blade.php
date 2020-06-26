63a6fb7640287166790876ef76c77bd7c83a7a99
Thomas Bockhorn <thomasbockhorn@Thomass-MBP.attlocal.net>
2020-06-26 10:35:21 -0500

    fixed breadcrumb css

--- resources/views/Backend_Post_Show.blade.php
+++ resources/views/Backend_Post_Show.blade.php
@@ -5,7 +5,9 @@
     <div class="col-12">
         <div class="card">
             <div class="card-block">
-                More details...
+                <h3>{{ $Blog_Post->title }}</h3>
+                <h6>{{ $Blog_Post->author }}</h6>
+                <date>{{ $Blog_Post->created_at->format('d-m-Y') }}<date>
             </div>
         </div>
     </div>

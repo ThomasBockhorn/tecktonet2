69f90ccce7cc2d419e6db6d1a4351d8c74bf0521
Thomas Bockhorn <thomasbockhorn@Thomass-MBP.attlocal.net>
2020-07-11 21:46:41 -0500

    Created Image model and migration

---
+++ database/migrations/2020_07_12_023844_create_images_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->unsignedBigInteger('post_id');
+            $table->timestamps();
+
+            //Foreign keys
+            $table->foreign('post_id')->references('id')->on('blog_posts')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::table('images', function (Blueprint $table) {
+            $table->dropForeign('post_image_foreign');
+        });
+
+        Schema::dropIfExists('images');
+    }
+}

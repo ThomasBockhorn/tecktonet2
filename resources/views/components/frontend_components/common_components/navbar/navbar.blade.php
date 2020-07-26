ddc41f0e3a5daa690d388125c60db40772fde96c
Thomas Bockhorn <thomasbockhorn@Thomass-MBP.attlocal.net>
2020-07-26 13:25:43 -0500

    Added navbar link to frontend projects

--- resources/views/components/frontend_components/common_components/navbar/navbar.blade.php
+++ resources/views/components/frontend_components/common_components/navbar/navbar.blade.php
@@ -1,37 +1,38 @@
 <!-- Header Area -->
 <header id="header" class="header">
-			<!-- Header Inner -->
-			<div class="header-inner">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-3 col-sm-4 col-xs-6 logo">
-							<a href="/"><img width="100%" height="100%" src="{{ asset('img/tecktonet_logo.png')}}" alt="logo.png"></a>
-						</div>
-						<div class="col-md-9 col-sm-8 col-xs-6">
-							<div class="mobile-menu"></div>
-							<nav class="navbar navbar-default">
-								<div class="collapse navbar-collapse">
-									<ul id="nav" class="nav navbar-nav">
-										<li><a href="/#Services">Services</a></li>
-										<li><a href="/#latest-works">portfolio<i class="fa fa-caret-down" aria-hidden="true"></i></a>
-											<ul class="dropdown">
-												<li><a href="portfolio.html">Portfolio</a></li>
-												<li><a href="portfolio-single.html">portfolio-single</a></li>
-											</ul>
-										</li>
-										<li><a href="/#blog">Blogs<i class="fa fa-caret-down" aria-hidden="true"></i></a>
-											<ul class="dropdown">
-												<li><a href="/blog">Blog</a></li>
-											</ul>
-										</li>
-										<li><a href="/#team">About Me</a></li>
-										<li><a href="/#contact">Contact</a></li>
-									</ul>
-								</div>
-							</nav>
-						</div>
-					</div>
-				</div>
-			</div>
-		</header>
-		<!--/ End Header Area -->
+    <!-- Header Inner -->
+    <div class="header-inner">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-4 col-xs-6 logo">
+                    <a href="/"><img width="100%" height="100%" src="{{ asset('img/tecktonet_logo.png')}}"
+                            alt="logo.png"></a>
+                </div>
+                <div class="col-md-9 col-sm-8 col-xs-6">
+                    <div class="mobile-menu"></div>
+                    <nav class="navbar navbar-default">
+                        <div class="collapse navbar-collapse">
+                            <ul id="nav" class="nav navbar-nav">
+                                <li><a href="/#Services">Services</a></li>
+                                <li><a href="/#latest-works">portfolio<i class="fa fa-caret-down"
+                                            aria-hidden="true"></i></a>
+                                    <ul class="dropdown">
+                                        <li><a href="/projects">Portfolio</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="/#blog">Blogs<i class="fa fa-caret-down" aria-hidden="true"></i></a>
+                                    <ul class="dropdown">
+                                        <li><a href="/blog">Blog</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="/#team">About Me</a></li>
+                                <li><a href="/#contact">Contact</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!--/ End Header Area -->

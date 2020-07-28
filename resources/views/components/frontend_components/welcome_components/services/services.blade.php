85ef06ada984b125340e9d65e8a2fbce136db030
Thomas Bockhorn <thomasbockhorn@Thomass-MBP.attlocal.net>
2020-07-28 12:07:37 -0500

    fixed a word

--- resources/views/components/frontend_components/welcome_components/services/services.blade.php
+++ resources/views/components/frontend_components/welcome_components/services/services.blade.php
@@ -1,59 +1,65 @@
 <!-- Start Service area -->
 <section id="Services" class="section">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<div class="section-title">
-							<h2>Services</h2>
-							<p>Tecktonet.com can provide these valuable services to facilitate your website.</p>
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.4s">
-						<div class="single-service">
-							<i class="fa fa-briefcase" aria-hidden="true"></i>
-							<h3>Responive Design</h3>
-							<p>Building a website that can be viewed in multiple viewports.  This will give your website a wider reach.  Viewers will be able to use your site on a phone as on a desktop.</p>
-						</div>
-					</div>
-					<div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.5s">
-						<div class="single-service">
-							<i class="fas fa-code" aria-hidden="true"></i>
-							<h3>Web Development</h3>
-							<p>Unlike content management systems that offer prebuilt solutions, I can build a website to your exact specifications.  Tecktonet.com custom solution will help you with your website needs.</p>
-						</div>
-					</div>
-					<div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.6s">
-						<div class="single-service">
-							<i class="fab fa-wordpress-simple" aria-hidden="true"></i>
-							<h3>WordPress Custom Themes</h3>
-							<p>Tecktonet.com can build WordPress themes if you want a custom solution for your blog or WooCommerce site.  All custom WordPress solutions will be responsive and easy to maintain.</p>
-						</div>
-					</div>
-					<div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.7s">
-					<div class="single-service">
-							<i class="fas fa-cash-register" aria-hidden="true"></i>
-							<h3>Ecommerce solution</h3>
-							<p>From a simple landing page to a complex ecommerce store, Tecktonet.com can provide you a great solution to reach new customers and generate better revenue for your business. </p>
-						</div>
-					</div>
-					<div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.8s">
-						<div class="single-service">
-							<i class="fa fa-globe" aria-hidden="true"></i>
-							<h3>SEO Marketing</h3>
-							<p>Tecktonet.com will provide a marketing campaign to entice foot traffic to your new site.  With a montly fee, I will place adds on social media like Facebook to make your website shine.</p>
-						</div>
-					</div>
-					<div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.9s">
-						<div class="single-service">
-							<i class="fas fa-laptop-code" aria-hidden="true"></i>
-							<h3>Maintaince/Hosting</h3>
-							<p>For a monthly free, Tecktonet.com will maintain your site so that your website will always be at your best.  I will also provide hosting so your website has a solid footing.</p>
-						</div>
-					</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-title">
+                    <h2>Services</h2>
+                    <p>Tecktonet.com can provide these valuable services to facilitate your website.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.4s">
+                <div class="single-service">
+                    <i class="fa fa-briefcase" aria-hidden="true"></i>
+                    <h3>Responsive Design</h3>
+                    <p>Building a website that can be viewed in multiple viewports. This will give your website a wider
+                        reach. Viewers will be able to use your site on a phone as on a desktop.</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.5s">
+                <div class="single-service">
+                    <i class="fas fa-code" aria-hidden="true"></i>
+                    <h3>Web Development</h3>
+                    <p>Unlike content management systems that offer prebuilt solutions, I can build a website to your
+                        exact specifications. Tecktonet.com custom solution will help you with your website needs.</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.6s">
+                <div class="single-service">
+                    <i class="fab fa-wordpress-simple" aria-hidden="true"></i>
+                    <h3>WordPress Custom Themes</h3>
+                    <p>Tecktonet.com can build WordPress themes if you want a custom solution for your blog or
+                        WooCommerce site. All custom WordPress solutions will be responsive and easy to maintain.</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.7s">
+                <div class="single-service">
+                    <i class="fas fa-cash-register" aria-hidden="true"></i>
+                    <h3>Ecommerce solution</h3>
+                    <p>From a simple landing page to a complex ecommerce store, Tecktonet.com can provide you a great
+                        solution to reach new customers and generate better revenue for your business. </p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.8s">
+                <div class="single-service">
+                    <i class="fa fa-globe" aria-hidden="true"></i>
+                    <h3>SEO Marketing</h3>
+                    <p>Tecktonet.com will provide a marketing campaign to entice foot traffic to your new site. With a
+                        montly fee, I will place adds on social media like Facebook to make your website shine.</p>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.9s">
+                <div class="single-service">
+                    <i class="fas fa-laptop-code" aria-hidden="true"></i>
+                    <h3>Maintaince/Hosting</h3>
+                    <p>For a monthly free, Tecktonet.com will maintain your site so that your website will always be at
+                        your best. I will also provide hosting so your website has a solid footing.</p>
+                </div>
+            </div>
 
-				</div>
-			</div>
-		</section>
-		<!-- End Service -->
+        </div>
+    </div>
+</section>
+<!-- End Service -->

b7a788d4ef887955a9fb19113ba1e295f551db38
Thomas Bockhorn <thomasbockhorn@Thomass-MBP.attlocal.net>
2020-07-30 11:22:38 -0500

    Added category model and migration

---
+++ database/migrations/2020_07_30_154838_create_categories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('category');
+            $table->unsignedBigInteger('project_id');
+            $table->timestamps();
+
+            //Foreign keys
+            $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('category', function (Blueprint $table) {
+            $table->dropForeign('project_id_foreign');
+        });
+
+        Schema::dropIfExists('categories');
+    }
+}

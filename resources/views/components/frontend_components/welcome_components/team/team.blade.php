01c01e05985d6cdcfa0174f8e2fe0669285fba6b
Thomas Bockhorn <thomasbockhorn@Thomass-MBP.attlocal.net>
2020-08-30 12:26:44 -0500

    Added my image and a small bio

--- resources/views/components/frontend_components/welcome_components/team/team.blade.php
+++ resources/views/components/frontend_components/welcome_components/team/team.blade.php
@@ -1,39 +1,60 @@
 <!-- Team -->
 <section id="team" class="section">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12 col-sm-12">
-						<div class="section-title">
-							<h2>About Me</h2>
-							<p>The man behind Tecktonet.com</p>
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-3 col-sm-6 col-xs-12 wow fadeInRight" data-wow-duration="0.8s" data-wow-delay="0.7s">
-						<!-- Single Team -->
-						<div class="single-team">
-							<div class="team-head">
-								<img src="img/team1.png" alt="">
-								<div class="team-hover">
-									<ul class="social">
-										<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-										<li><a href="#"><i class="fab fa-twitter"></i></a></li>
-										<li><a href="#"><i class="fab fa-instagram"></i></a></li>
-										<li><a href="#"><i class="fab fa-linkedin"></i></a></li>
-									</ul>
-								</div>
-							</div>
-							<div class="t-name">
-								<h4>Thomas Bockhorn<span>Web Developer</span></h4>
-							</div>
-						</div>
-						<!--/ End Single Team -->
-					</div>
-					<div class="col-md-3 col-sm-6 col-xs-12 wow fadeInRight" data-wow-duration="0.8s" data-wow-delay="0.7s">
-						<p>This is a test</p>
-					</div>
-				</div>
-			</div>
-		</section>
-		<!--/ End Team -->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-sm-12">
+                <div class="section-title">
+                    <h2>About Me</h2>
+                    <p>The man behind Tecktonet.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3 col-sm-6 col-xs-12 wow fadeInRight" data-wow-duration="0.8s" data-wow-delay="0.7s">
+                <!-- Single Team -->
+                <div class="single-team">
+                    <div class="team-head">
+                        <img src="img/ThomasBockhorn.jpg" alt="">
+                        <div class="team-hover">
+                            <ul class="social">
+                                <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="t-name">
+                        <h4>Thomas Bockhorn<span>Web Developer</span></h4>
+                    </div>
+                </div>
+                <!--/ End Single Team -->
+            </div>
+            <div class="col-md-9 col-sm-6 col-xs-12 wow fadeInRight" data-wow-duration="0.8s" data-wow-delay="0.7s">
+                <p>Hi</p>
+                <br>
+                <p> Building websites are fun and challanging. Websites can also be creative endevors that inspire. </p>
+                <br>
+                <p> I first got into web development with an invitation by Udacity including a nice Google scholarship
+                    for their front-end web development program. After taking a few lessions, I became hooked on all
+                    things web development. To further my education, I took and continue to take Udemy courses for both
+                    front-end and back-end development. The beauty of both technical and asthetic spheres and how they
+                    interacted together fascinated me. Just like architecture, good websites need to consider both form
+                    and function.</p>
+                <br>
+                <p> My other source of inspiration is history and the art of telling stories. I received a Master degree
+                    in history. Good historical narratives and websites share how important the central theme is and how
+                    essential the human connection is. History and websites can be dry and boring, or can be interesting
+                    and compelling. It all depends on the content. I try to make compelling websites that garner
+                    viewer's attention.</p>
+                <br>
+                <p> Let me help you build a website that will keep bringing in vistors, that will provide a compelling
+                    narrative, that will be easy for a vistor to use, and that will be clean and asthetically pleasing.
+                    Let's build a website that you can be proud of.</p>
+                <br>
+                <p> -Thomas Bockhorn</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!--/ End Team -->

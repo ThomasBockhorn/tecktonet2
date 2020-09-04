c13a1a64e62489caed1bb1c849386b466bd5c3a9
Thomas Bockhorn <thomasbockhorn@Thomass-MBP.attlocal.net>
2020-09-04 18:29:29 -0500

    made the images in features work

--- resources/views/components/frontend_components/welcome_components/features/features.blade.php
+++ resources/views/components/frontend_components/welcome_components/features/features.blade.php
@@ -1,53 +1,56 @@
 <!-- features Area -->
 <section id="features" class="section">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<div class="section-title">
-							<h2>Features of a great website</h2>
-							<p>The best websites have the following characteristics</p>
-						</div>
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-6 col-sm-12 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.9s">
-						<div class="feature-left">
-							<img src="img/computer.jpg" alt="#">
-						</div>	
-					</div>
-					<div class="col-md-6 col-sm-12 col-xs-12 wow fadeInRight" data-wow-duration="0.8s" data-wow-delay="0.9s">
-						<div class="feature-right">
-							<div class="single-right">
-								<i class="fas fa-laptop" aria-hidden="true"></i>
-								<div class="right-content">
-									<h3>UX UI design</h3>
-									<P>A great website will be valuable to both consumer and client.  Consumers get what they need quickly.  Clients will get a steady stream of consumers to their site.</P>
-								</div>
-							</div>
-							<div class="single-right">
-								<i class="fa fa-database" aria-hidden="true"></i>
-								<div class="right-content">
-									<h3>Database design</h3>
-									<P>If a website requires a database, the database is simple and easy to maintain.</P>
-								</div>
-							</div>
-							<div class="single-right">
-								<i class="fa fa-cubes" aria-hidden="true"></i>
-								<div class="right-content">
-									<h3>SEO Services</h3>
-									<P>A great website also needs a great online marketing campaign to draw traffic to a site.  Ads on Facebook is just an example.</P>
-								</div>
-							</div>
-							<div class="single-right">
-								<i class="fa fa-desktop" aria-hidden="true"></i>
-								<div class="right-content">
-									<h3>Web Development</h3>
-									<P>A great website will be build with a clean, well thought through code base.  That way, a website is easier to maintain over time.</P>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-		<!--/ End features Area -->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-title">
+                    <h2>Features of a great website</h2>
+                    <p>The best websites have the following characteristics</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 col-sm-12 col-xs-12 wow fadeInLeft" data-wow-duration="0.8s" data-wow-delay="0.9s">
+                <div class="feature-left">
+                    <img src="{{asset('../img/computer.jpg')}}" alt="#">
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-12 col-xs-12 wow fadeInRight" data-wow-duration="0.8s" data-wow-delay="0.9s">
+                <div class="feature-right">
+                    <div class="single-right">
+                        <i class="fas fa-laptop" aria-hidden="true"></i>
+                        <div class="right-content">
+                            <h3>UX UI design</h3>
+                            <P>A great website will be valuable to both consumer and client. Consumers get what they
+                                need quickly. Clients will get a steady stream of consumers to their site.</P>
+                        </div>
+                    </div>
+                    <div class="single-right">
+                        <i class="fa fa-database" aria-hidden="true"></i>
+                        <div class="right-content">
+                            <h3>Database design</h3>
+                            <P>If a website requires a database, the database is simple and easy to maintain.</P>
+                        </div>
+                    </div>
+                    <div class="single-right">
+                        <i class="fa fa-cubes" aria-hidden="true"></i>
+                        <div class="right-content">
+                            <h3>SEO Services</h3>
+                            <P>A great website also needs a great online marketing campaign to draw traffic to a site.
+                                Ads on Facebook is just an example.</P>
+                        </div>
+                    </div>
+                    <div class="single-right">
+                        <i class="fa fa-desktop" aria-hidden="true"></i>
+                        <div class="right-content">
+                            <h3>Web Development</h3>
+                            <P>A great website will be build with a clean, well thought through code base. That way, a
+                                website is easier to maintain over time.</P>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--/ End features Area -->

bc589a356a3563573d74386ce009dc72b69a39a1
thomas <historian.thomas@gmail.com>
2020-09-20 16:37:26 -0500

    minor fix

--- resources/views/frontend_pages/Mail-Success.blade.php
+++ resources/views/frontend_pages/Mail-Success.blade.php
@@ -3,27 +3,27 @@
 @section('content')
 <!-- mail-success Page -->
 <section class="mail-success section">
-			<div class="table">
-				<div class="table-cell">
-					<div class="container">
-						<div class="row">
-							<div class="col-md-12 col-sm-12 col-xs-12">
-								<div class="mail-head">
-									<img src="img/success.png" alt="#">
-									<h2><span><img src="img/tic.png" alt="#"></span>Your Mail Has Sent Successfully!</h2>
-									<div class="button-head">
-										<div class="button">
-											<span></span>
-											<a href="/" class="btn">Go Back</a>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-		<!-- End mail-success Page -->
+    <div class="table">
+        <div class="table-cell">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="mail-head">
+                            <img src="img/success.png" alt="#">
+                            <h2><span><img src="img/tic.png" alt="#"></span>Your Mail Has Sent Successfully!</h2>
+                            <div class="button-head">
+                                <div class="button">
+                                    <span></span>
+                                    <a href="/" class="btn">Go Back</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End mail-success Page -->
 
 @endsection

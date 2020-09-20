f89b44623eb65eb516e5a3f45a81d408f586b042
thomas <historian.thomas@gmail.com>
2020-09-20 16:43:18 -0500

    minor fix

--- resources/views/components/backend_components/navbar/navbar.blade.php
+++ resources/views/components/backend_components/navbar/navbar.blade.php
@@ -1,52 +1,55 @@
 <!-- ============================================================== -->
-        <!-- Topbar header - style you can find in pages.scss -->
+<!-- Topbar header - style you can find in pages.scss -->
+<!-- ============================================================== -->
+<header class="topbar">
+    <nav class="navbar top-navbar navbar-toggleable-sm navbar-light sticky-top">
         <!-- ============================================================== -->
-        <header class="topbar">
-            <nav class="navbar top-navbar navbar-toggleable-sm navbar-light sticky-top">
+        <!-- Logo -->
+        <!-- ============================================================== -->
+        <div class="navbar-header">
+            <a class="navbar-brand" href="{{ url('/home') }}">
+                <!-- Logo icon --><b>
+                    <!-- Light Logo icon -->
+                    <img height="50px" width="50px" src="{{ asset('img/tecktonet_icon.png')}}" alt="homepage"
+                        class="light-logo" />
+                </b>
+                <!--End Logo icon -->
+                <!-- Logo text --><span>
+
+                    <!-- Light Logo text -->
+                    <img height="50px" width="200px" src="{{ asset('img/tecktonet_text.png')}}" class="light-logo"
+                        alt="homepage" /></span> </a>
+        </div>
+        <ul class="navbar-nav mr-auto mt-md-0">
+            <!-- This is  -->
+            <li class="nav-item"> <a class="nav-link nav-toggler hidden-md-up text-muted waves-effect waves-dark"
+                    href="javascript:void(0)"><i class="fas fa-bars"></i></a> </li>
+        </ul>
+        <!-- ============================================================== -->
+        <div class="navbar-collapse">
+            <ul class="navbar-nav ml-auto my-lg-0">
                 <!-- ============================================================== -->
-                <!-- Logo -->
+                <!-- Profile -->
                 <!-- ============================================================== -->
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="{{ url('/home') }}">
-                        <!-- Logo icon --><b>
-                            <!-- Light Logo icon -->
-                            <img height="50px" width="50px" src="{{ asset('img/tecktonet_icon.png')}}" alt="homepage" class="light-logo" />
-                        </b>
-                        <!--End Logo icon -->
-                        <!-- Logo text --><span>
-                         
-                         <!-- Light Logo text -->    
-                         <img height="50px" width="200px" src="{{ asset('img/tecktonet_text.png')}}" class="light-logo" alt="homepage" /></span> </a>
-                </div>
-                <ul class="navbar-nav mr-auto mt-md-0">
-                <!-- This is  -->
-                    <li class="nav-item"> <a class="nav-link nav-toggler hidden-md-up text-muted waves-effect waves-dark" href="javascript:void(0)"><i class="fas fa-bars"></i></a> </li>
-                </ul>
-                        <!-- ============================================================== -->
-                <div class="navbar-collapse">
-                    <ul class="navbar-nav ml-auto my-lg-0">
-                        <!-- ============================================================== -->
-                        <!-- Profile -->
-                        <!-- ============================================================== -->
-                        @guest
-                            <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
-                            <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="nav-item">
-                                <a class="nav-link" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
+                @guest
+                <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
+                @else
+                <li class="nav-item">
+                    <a class="nav-link" href="" data-toggle="dropdown" aria-haspopup="true"
+                        aria-expanded="false">{{ Auth::user()->name }}</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
                                     document.getElementById('logout-form').submit();">
-                                    Logout
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
-                             </li>
-                        @endguest
-                    </ul>
-                </div>
-            </nav>
-        </header>
+                        Logout
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
+                </li>
+                @endguest
+            </ul>
+        </div>
+    </nav>
+</header>
